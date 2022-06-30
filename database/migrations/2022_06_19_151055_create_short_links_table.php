21d049aa3a1e0a6e49666e5171d91b9e3b147a2c
taherbth <tahersumonabu@gmail.com>
2022-07-01 01:33:53 +0600

    Some cleanup done...

---
+++ database/migrations/2022_06_19_151055_create_short_links_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShortLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('short_links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('long_url');
+            $table->string('short_code')->nullable();
+            $table->integer('number_of_hit')->default('0');
+            $table->integer('custom_short_code')->default('0');            
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('short_links');
+    }
+}
